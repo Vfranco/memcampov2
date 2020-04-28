fe3e6bdb73b86775493f5171542de575db709c68
Victor Franco <vfrancov25@gmail.com>
2020-04-28 00:28:29 -0500

    Ajustando Administrator

--- src/app/dashboard/administrator/administrator.module.ts
+++ src/app/dashboard/administrator/administrator.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AuthGuard } from 'src/app/guards/auth.guard';
-
-import { CiclodevidaComponent } from './ciclodevida/ciclodevida.component';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from 'src/app/guards/auth.guard';
+import { CiclodevidaComponent } from './ciclodevida/ciclodevida.component';
 
 const adminRoutes : Routes = [
 	{ path : 'dashboard/administrator/ciclodevida', component: CiclodevidaComponent, canActivate: [AuthGuard] }
