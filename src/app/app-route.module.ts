8da026ce74ca409dab5f0548a01657f2332a4592
Victor Franco <vfrancov25@gmail.com>
2020-04-27 21:50:48 -0500

    Definiendo Vistas Login, Registro, Recovery

--- src/app/app-route.module.ts
+++ src/app/app-route.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { Routes, RouterModule, PreloadAllModules, Router } from '@angular/router';
 
-
+const appRoute: Routes = [
+	{ path: '', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+	{ path: '**', redirectTo: '', pathMatch: 'full' }
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+	declarations: [],
+	imports: [
+		CommonModule,
+		HttpClientModule,
+		RouterModule.forRoot(appRoute, {
+			enableTracing : false,
+			preloadingStrategy : PreloadAllModules
+		})
+	],
+	exports : [RouterModule]
 })
 export class AppRouteModule { }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { RecoveryComponent } from './recovery/recovery.component';
 
-
+const authRoute : Routes = [
+	{ path : '', component : LoginComponent },
+	{ path : 'register', component : RegisterComponent },
+	{ path : 'recovery', component : RecoveryComponent }
+]
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, RecoveryComponent],
-  imports: [
-    CommonModule
-  ]
+	declarations: [
+		LoginComponent,
+		RegisterComponent,
+		RecoveryComponent
+	],
+	imports: [
+		CommonModule,
+		RouterModule.forChild(authRoute)
+	]
 })
 export class AuthModule { }
