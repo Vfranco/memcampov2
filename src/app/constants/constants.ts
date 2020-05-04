e8ae0074bc8dbefc706c8285e267442608eced02
Diego Ramirez <dialrasa2@gmail.com>
2020-05-04 11:37:54 -0500

    Actualizando coleccion constants
    Se añadio CICLOS_DE_VIDA

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -1,6 +1,7 @@
 //This file set all the collections
 
-export const collections: any =  {
-    USUARIOS    : "usuarios",
-    FINCAS      : "fincas"
+export const collections: any = {
+    USUARIOS: "usuarios",
+    FINCAS: "fincas",
+    CICLOS_DE_VIDA: "ciclos_de_vida"
 };
