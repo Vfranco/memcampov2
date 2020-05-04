c2ca365cf47a2b717ca63866aeb7ad1c3d936a7b
Victor Franco <vfrancov25@gmail.com>
2020-05-03 22:23:51 -0500

    Ajustes en Login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { Login } from 'src/app/models/login.model';
-import { AngularFireAuth } from '@angular/fire/auth';
 import { AuthService } from 'src/app/services/auth.service';
 import { LocalstorageService } from 'src/app/services/localstorage.service';
 import { firebaseErrors } from 'src/app/constants/firebase.errors';
