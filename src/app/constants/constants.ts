3b8a35656c6dd4385ca390e8b7802ff915e56958
Diego Ramirez <dialrasa2@gmail.com>
2020-05-04 17:30:39 -0500

    Adicion de las colecciones y cambio de model a interface en login
    Se añadieron las colecciones restantes en el constants y se reemplazo el modelo por una interface para el login

--- src/app/constants/constants.ts
+++ src/app/constants/constants.ts
@@ -3,5 +3,21 @@
 export const collections: any = {
     USUARIOS: "usuarios",
     FINCAS: "fincas",
-    CICLOS_DE_VIDA: "ciclos_de_vida"
+    ACTIVIDADES: "actividades",
+    CICLOS_DE_VIDA: "ciclos_de_vida",
+    COSTOS: "costos",
+    CULTIVOS: "cultivos",
+    DEPARTAMENTOS: "departamentos",
+    LOTES: "lotes",
+    MAQUINARIA: "maquinaria",
+    MENUROLES: "menuRoles",
+    PRODUCTOS: "productos",
+    TIPOS_DE_ACTIVIDAD: "tipos_de_actividad",
+    TIPOS_DE_COSTO: "tipos_de_costo",
+    TIPOS_DE_CULTIVO: "tipos_de_cultivos",
+    TIPOS_DE_PRODUCTOS: "tipos_de_productos",
+    UNIDADES_DE_COBRO: "unidades_de_cobro",
+    UNIDADES_DE_MEDIDA: "unidades_de_medida",
+    UNIDADES_DE_SUPERFICIE: "unidades_de_superficie",
+    USERSROLES: "usersRoles",
 };
---
+++ src/app/interface/login.interface.ts
@@ -0,0 +1,6 @@
+/** Login interface */
+
+export interface Login {
+    email: string;
+    pass: string;
+}
