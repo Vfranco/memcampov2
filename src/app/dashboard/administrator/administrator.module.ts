a32bcf9cec593255cd56685bbe441ccc2f937990
Diego RS <dialrasa2@gmail.com>
2020-05-16 19:17:34 -0500

    Avances ciclos de vida
    Creacion de un modulo para ciclos de vida.

--- src/app/dashboard/administrator/administrator.module.ts
+++ src/app/dashboard/administrator/administrator.module.ts
@@ -3,19 +3,17 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AuthGuard } from 'src/app/guards/auth.guard';
-import { CiclodevidaComponent } from './ciclodevida/ciclodevida.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { TiposcultivosComponent } from './tiposcultivos/tiposcultivos.component';
 
-const adminRoutes : Routes = [
-	{ path : 'ciclos', component: CiclodevidaComponent, canActivate: [AuthGuard] },
-	{ path : 'usuarios', component: UsuariosComponent, canActivate: [AuthGuard] },
-	{ path : 'tiposcultivo', component: TiposcultivosComponent, canActivate : [AuthGuard] }
+const adminRoutes: Routes = [
+	{ path: '', loadChildren: () => import('./ciclosdevida/ciclosdevida.module').then(m => m.CiclosdevidaModule) },
+	{ path: 'usuarios', component: UsuariosComponent, canActivate: [AuthGuard] },
+	{ path: 'tiposcultivo', component: TiposcultivosComponent, canActivate: [AuthGuard] }
 ]
 
 @NgModule({
 	declarations: [
-		CiclodevidaComponent,
 		UsuariosComponent,
 		TiposcultivosComponent
 	],
